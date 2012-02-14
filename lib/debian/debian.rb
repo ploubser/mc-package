a939de0455074532c3400dc895cf0b73ab2e5bfa
R.I.Pienaar <rip@devco.net>
2012-02-14 15:58:30 +0000

    - White space changes - Use File.join() to be more portable

--- lib/debian/debian.rb
+++ lib/debian/debian.rb
@@ -1,35 +1,34 @@
 module MCPackage
-    class Debian < Package
+  class Debian < Package
 
-        def initialize(name, version, post_install)
-            mc_path = "usr/libexec/mcollective/mcollective/"
-            super(name, version, mc_path, post_install)
-        end
-
-        def create_package
-            #TODO: Deal with fpm output
-            create_dependencies if @dependencies
-            FPM::Program.new.run params("agent") if @agent
-            FPM::Program.new.run params("application") if @application
-        end
-
-        def create_dependencies
-            FPM::Program.new.run dep_params
-        end
-
-        def params(dir)
-            #Standard fpm flags
-            params = ["-s", "dir", "-C", @tmp_dir, "-t", "deb", "-a", "all",
-                "-n", "#{@name}-#{dir}", "-v", @version]
-            #Post install scripts
-            params += ["-d","#{@name}-common"] if @dependencies
-            params += ["--post-install", @post_install] if @post_install
-            params << "#{@mc_path}#{dir}/"
-        end
-
-        def dep_params
-            params = ["-s", "dir", "-C", @tmp_dir, "-t", "deb", "-a", "all",
-                "-n", "#{@name}-common", "-v", @version, "#{@mc_path}common/"]
-        end
+    def initialize(name, version, post_install)
+      mc_path = "usr/libexec/mcollective/mcollective/"
+      super(name, version, mc_path, post_install)
     end
+
+    def create_package
+      #TODO: Deal with fpm output
+      create_dependencies if @dependencies
+      FPM::Program.new.run params("agent") if @agent
+      FPM::Program.new.run params("application") if @application
+    end
+
+    def create_dependencies
+      FPM::Program.new.run dep_params
+    end
+
+    def params(dir)
+      #Standard fpm flags
+      params = ["-s", "dir", "-C", @tmp_dir, "-t", "deb", "-a", "all", "-n", "#{@name}-#{dir}", "-v", @version]
+
+      #Post install scripts
+      params += ["-d","#{@name}-common"] if @dependencies
+      params += ["--post-install", @post_install] if @post_install
+      params << File.join(@mc_path, dir)
+    end
+
+    def dep_params
+      params = ["-s", "dir", "-C", @tmp_dir, "-t", "deb", "-a", "all", "-n", "#{@name}-common", "-v", @version, File.join(@mc_path, "common")]
+    end
+  end
 end
